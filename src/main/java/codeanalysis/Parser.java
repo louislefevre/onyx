86615e2320c5ff009d35034ae9eb1bc908aee466
Louis Lefevre <LouisLefev@gmail.com>
2020-02-03 22:50:54 +0000

    Update Parser.java
    Reformatted Parser.java class to be more modular and organised.

--- src/main/java/codeanalysis/Parser.java
+++ src/main/java/codeanalysis/Parser.java
@@ -6,76 +6,54 @@ import java.util.List;
 public class Parser
 {
     private Lexer lexer;
-    private SyntaxToken token;
     private List<SyntaxToken> tokens;
-    private List<String> diagnostics;
+    private List<String> diagnosticsLog;
     private int position;
 
     public Parser(Lexer lexer)
     {
         this.lexer = lexer;
         this.tokens = new ArrayList<>();
-        this.diagnostics = new ArrayList<>();
+        this.diagnosticsLog = new ArrayList<>();
+        this.position = 0;
+        this.addTokens();
+        this.addDiagnostics();
+    }
 
+    private void addTokens()
+    {
+        SyntaxToken token;
         do
         {
-            this.token = this.lexer.nextToken();
-
-            if(this.token.getKind() != SyntaxKind.WhiteSpace && this.token.getKind() != SyntaxKind.BadToken)
-                this.tokens.add(this.token);
-
-        } while(this.token.getKind() != SyntaxKind.EndOfFileToken);
-
-        this.diagnostics.addAll(this.lexer.getDiagnostics());
+            token = this.lexer.nextToken();
+            if(token.getKind() != SyntaxKind.BadToken && token.getKind() != SyntaxKind.WhiteSpace)
+                this.tokens.add(token);
+        }while(token.getKind() != SyntaxKind.EndOfFileToken);
     }
 
-    public List<String> getDiagnostics() { return this.diagnostics; }
-
-    private SyntaxToken peek(int offset)
+    private void addDiagnostics()
     {
-        int index = this.position + offset;
-        if(index >= this.tokens.size())
-            return this.tokens.get(this.tokens.size() - 1);
-        return this.tokens.get(index);
-    }
-
-    private SyntaxToken current() { return peek(0); }
-
-    private SyntaxToken nextToken()
-    {
-        SyntaxToken current = this.current();
-        this.position++;
-        return current;
+        this.diagnosticsLog.addAll(this.lexer.getDiagnosticsLog());
     }
 
     private SyntaxToken match(SyntaxKind kind)
     {
-        if(this.current().getKind() == kind)
+        if(this.currentToken().getKind() == kind)
             return this.nextToken();
-        this.diagnostics.add(String.format("ERROR: Unexpected token '%1s', expected '%2s'", this.current().getKind(), kind));
-        return new SyntaxToken(kind, this.current().getPosition(), null, null);
+        this.diagnosticsLog.add(String.format("ERROR: Unexpected token '%1s', expected '%2s'", this.currentToken().getKind(), kind));
+        return new SyntaxToken(kind, this.currentToken().getPosition(), null, null);
     }
 
     private ExpressionSyntax parseExpression()
     {
-        return parseTerm();
-    }
-
-    public ExpressionSyntax getExpression()
-    {
         return this.parseTerm();
     }
 
-    public SyntaxToken getEndOfFileToken()
-    {
-        return this.match(SyntaxKind.EndOfFileToken);
-    }
-
-    public ExpressionSyntax parseTerm()
+    private ExpressionSyntax parseTerm()
     {
         ExpressionSyntax left = this.parseFactor();
 
-        while(this.current().getKind() == SyntaxKind.PlusToken || this.current().getKind() == SyntaxKind.MinusToken)
+        while(this.currentToken().getKind() == SyntaxKind.PlusToken || this.currentToken().getKind() == SyntaxKind.MinusToken)
         {
             SyntaxToken operatorToken = this.nextToken();
             ExpressionSyntax right = this.parseFactor();
@@ -85,11 +63,11 @@ public class Parser
         return left;
     }
 
-    public ExpressionSyntax parseFactor()
+    private ExpressionSyntax parseFactor()
     {
         ExpressionSyntax left = this.parsePrimaryExpression();
 
-        while(this.current().getKind() == SyntaxKind.StarToken || this.current().getKind() == SyntaxKind.SlashToken)
+        while(this.currentToken().getKind() == SyntaxKind.StarToken || this.currentToken().getKind() == SyntaxKind.SlashToken)
         {
             SyntaxToken operatorToken = this.nextToken();
             ExpressionSyntax right = this.parsePrimaryExpression();
@@ -101,7 +79,7 @@ public class Parser
 
     private ExpressionSyntax parsePrimaryExpression()
     {
-        if(this.current().getKind() == SyntaxKind.OpenParenthesisToken)
+        if(this.currentToken().getKind() == SyntaxKind.OpenParenthesisToken)
         {
             SyntaxToken left = this.nextToken();
             ExpressionSyntax expression = this.parseExpression();
@@ -111,5 +89,35 @@ public class Parser
 
         SyntaxToken numberToken = this.match(SyntaxKind.NumberToken);
         return new NumberExpressionSyntax(numberToken);
+    }
+
+    private SyntaxToken nextToken()
+    {
+        SyntaxToken current = this.currentToken();
+        this.position++;
+        return current;
+    }
+
+    private SyntaxToken currentToken()
+    {
+        return peekToken(0);
+    }
+
+    private SyntaxToken peekToken(int peekPos)
+    {
+        int index = this.position + peekPos;
+        if(index >= this.tokens.size())
+            return this.tokens.get(this.tokens.size() - 1);
+        return this.tokens.get(index);
+    }
+
+    public List<String> getDiagnosticsLog()
+    {
+        return this.diagnosticsLog;
+    }
+
+    public ExpressionSyntax getExpression()
+    {
+        return this.parseExpression();
     }
 }
