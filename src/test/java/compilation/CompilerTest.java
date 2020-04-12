7c416ac1aea56090c9b5d2457a73b78f5abf9624
Louis Lefevre <louislefev@gmail.com>
2020-04-12 23:25:58 +0100

    Remove unused test classes

--- src/test/java/compilation/CompilerTest.java
+++
@@ -1,8 +0,0 @@
-package compilation;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CompilerTest
-{
-
-}
--- src/test/java/errors/ErrorHandlerTest.java
+++
@@ -1,8 +0,0 @@
-package errors;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ErrorHandlerTest
-{
-
-}
