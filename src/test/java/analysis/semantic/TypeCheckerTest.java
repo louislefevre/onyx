f10de97b086c3f8babc50b57c530616a792e44ab
Louis Lefevre <louislefev@gmail.com>
2020-04-12 23:24:27 +0100

    Add TypeChecker creator

--- src/test/java/analysis/semantic/TypeCheckerTest.java
+++ src/test/java/analysis/semantic/TypeCheckerTest.java
@@ -1,8 +1,22 @@
 package analysis.semantic;
 
+import analysis.lexical.Lexer;
+import analysis.syntax.Parser;
+import errors.ErrorHandler;
+import org.jetbrains.annotations.NotNull;
+import symbols.SymbolTable;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TypeCheckerTest
 {
-
+    @NotNull
+    private static TypeChecker createTypeChecker(String input)
+    {
+        SymbolTable symbolTable = new SymbolTable();
+        ErrorHandler errorHandler = new ErrorHandler(input);
+        Lexer lexer = new Lexer(input, errorHandler);
+        Parser parser = new Parser(lexer, errorHandler);
+        return new TypeChecker(parser, errorHandler, symbolTable);
+    }
 }
