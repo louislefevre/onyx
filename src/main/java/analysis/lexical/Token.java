1e8f3ccdbaee0a7b3d85287475f98324f7534292
Louis Lefevre <louislefev@gmail.com>
2020-04-24 00:39:25 +0100

    Remove Token overloads
    To help avoid hard to track bugs and make it clearer what values a Token has before being created, removed the overloaded constructors from Token as well as its helper methods.

--- src/main/java/analysis/lexical/Token.java
+++ src/main/java/analysis/lexical/Token.java
@@ -2,7 +2,6 @@ package analysis.lexical;
 
 import identifiers.TokenType;
 import lombok.Getter;
-import org.jetbrains.annotations.Nullable;
 import source.SourceSpan;
 
 @Getter
@@ -21,81 +20,5 @@ public final class Token
         this.value = value;
         this.position = position;
         this.span = new SourceSpan(position, syntax.length());
-    }
-
-    public Token(TokenType tokenType, String syntax, int position)
-    {
-        this(tokenType, syntax, findValue(tokenType), position);
-    }
-
-    public Token(TokenType tokenType, int position)
-    {
-        this(tokenType, findSyntax(tokenType), position);
-    }
-
-    @Nullable
-    private static Object findValue(TokenType tokenType)
-    {
-        switch (tokenType)
-        {
-            case TRUE_KEYWORD_TOKEN:
-                return true;
-            case FALSE_KEYWORD_TOKEN:
-                return false;
-            default:
-                return null;
-        }
-    }
-
-    @Nullable
-    private static String findSyntax(TokenType tokenType)
-    {
-        switch (tokenType)
-        {
-            case PLUS_TOKEN:
-                return Syntax.PLUS.getSyntax();
-            case MINUS_TOKEN:
-                return Syntax.MINUS.getSyntax();
-            case STAR_TOKEN:
-                return Syntax.STAR.getSyntax();
-            case SLASH_TOKEN:
-                return Syntax.SLASH.getSyntax();
-            case CARET_TOKEN:
-                return Syntax.CARET.getSyntax();
-            case PERCENT_TOKEN:
-                return Syntax.PERCENT.getSyntax();
-            case OPEN_PARENTHESIS_TOKEN:
-                return Syntax.OPEN_PARENTHESIS.getSyntax();
-            case CLOSE_PARENTHESIS_TOKEN:
-                return Syntax.CLOSE_PARENTHESIS.getSyntax();
-            case EQUALS_EQUALS_TOKEN:
-                return Syntax.EQUALS_EQUALS.getSyntax();
-            case EQUALS_TOKEN:
-                return Syntax.EQUALS.getSyntax();
-            case NOT_EQUALS_TOKEN:
-                return Syntax.NOT_EQUALS.getSyntax();
-            case NOT_TOKEN:
-                return Syntax.NOT.getSyntax();
-            case GREATER_TOKEN:
-                return Syntax.GREATER.getSyntax();
-            case LESS_TOKEN:
-                return Syntax.LESS.getSyntax();
-            case GREATER_EQUALS_TOKEN:
-                return Syntax.GREATER_EQUALS.getSyntax();
-            case LESS_EQUALS_TOKEN:
-                return Syntax.LESS_EQUALS.getSyntax();
-            case AND_TOKEN:
-                return Syntax.AND.getSyntax();
-            case OR_TOKEN:
-                return Syntax.OR.getSyntax();
-            case TRUE_KEYWORD_TOKEN:
-                return Syntax.TRUE.getSyntax();
-            case FALSE_KEYWORD_TOKEN:
-                return Syntax.FALSE.getSyntax();
-            case EOF_TOKEN:
-                return Syntax.EOF.getSyntax();
-            default:
-                return null;
-        }
     }
 }
