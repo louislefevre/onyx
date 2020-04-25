dfaefc3e48017acb85fadff36f457bb407c7c7c3
Louis Lefevre <louislefev@gmail.com>
2020-04-25 23:21:28 +0100

    Add HashMap to SymbolTable
    Instead of using an ArrayList, the SymbolTable now uses a HashMap.
    
    Also added value tracking to identifier tokens.

--- src/main/java/analysis/lexical/Lexer.java
+++ src/main/java/analysis/lexical/Lexer.java
@@ -112,7 +112,7 @@ public final class Lexer
         else if (Syntax.OR.getSyntax().equals(text))
             return new Token(TokenType.OR_TOKEN, Syntax.OR.getSyntax(), pos);
 
-        return new Token(TokenType.IDENTIFIER_KEYWORD_TOKEN, text, pos);
+        return new Token(TokenType.IDENTIFIER_KEYWORD_TOKEN, text, text, pos);
     }
 
     private Token stringToken()
