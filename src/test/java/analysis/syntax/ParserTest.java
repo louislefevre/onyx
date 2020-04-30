af1f27470c884aef98c854af4af711bb3dc8b3ef
Louis Lefevre <louislefev@gmail.com>
2020-04-30 20:28:24 +0100

    Add Parser tests
    Implemented tests for parser.

--- src/test/java/analysis/syntax/ParserTest.java
+++ src/test/java/analysis/syntax/ParserTest.java
@@ -1,232 +1,97 @@
 package analysis.syntax;
 
-import analysis.lexical.Syntax;
 import identifiers.ExpressionType;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import utilities.TestHub;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest
 {
     @Test
-    public void parserIdentifiesNumericalLiteralExpression()
+    public void parserIdentifiesLiteralExpression()
     {
-        ExpressionType expectedExpression = ExpressionType.LITERAL_EXPRESSION;
-        String message = "Failed to identify numerical literal expression: ";
+        String message = "Failed to identify literal expression: ";
+        HashMap<String, Object> dataCollection = TestHub.dataCollections();
 
-        int valueLimit = 1000;
-
-        for (int i = 0; i <= valueLimit; i++)
-            assertEquals(expectedExpression, expressionTypeOf(String.valueOf(i)), message + i);
+        dataCollection.forEach((input, redundant) -> {
+            ExpressionType actual = expressionTypeOf(input);
+            ExpressionType expected = ExpressionType.LITERAL_EXPRESSION;
+            assertEquals(expected, actual, message + input);
+        });
     }
 
     @Test
-    public void parserIdentifiesStringLiteralExpression()
+    public void parserIdentifiesUnaryExpression()
     {
-        ExpressionType expectedExpression = ExpressionType.LITERAL_EXPRESSION;
-        String message = "Failed to identify string literal expression: ";
+        String message = "Failed to identify unary expression: ";
+        HashMap<String, Object> unaryCollection = TestHub.unaryCollections();
 
-        String[] strings = {"\"a\"", "\"string\"", "\"separated string\"", "\"0\"",};
-
-        for (String str : strings)
-            assertEquals(expectedExpression, expressionTypeOf(str), message + str);
+        unaryCollection.forEach((input, redundant) -> {
+            ExpressionType actual = expressionTypeOf(input);
+            ExpressionType expected = ExpressionType.UNARY_EXPRESSION;
+            assertEquals(expected, actual, message + input);
+        });
     }
 
     @Test
-    public void parserIdentifiesConditionalLiteralExpression()
+    public void parserIdentifiesBinaryExpression()
     {
-        ExpressionType expectedExpression = ExpressionType.LITERAL_EXPRESSION;
-        String message = "Failed to identify numerical literal expression: ";
+        String message = "Failed to identify binary expression: ";
+        HashMap<String, Object> binaryCollection = TestHub.binaryCollections();
 
-        String[] keywords = {Syntax.TRUE.getSyntax(), Syntax.FALSE.getSyntax()};
-
-        for (String keyword : keywords)
-            assertEquals(expectedExpression, expressionTypeOf(keyword), message + keyword);
-    }
-
-    @Test
-    public void parserIdentifiesNumericalUnaryExpression()
-    {
-        ExpressionType expectedExpression = ExpressionType.UNARY_EXPRESSION;
-        String message = "Failed to identify numerical unary expression: ";
-
-        int valueLimit = 1000;
-        String[] unaryOperators = {Syntax.PLUS.getSyntax(), Syntax.MINUS.getSyntax()};
-
-        for (int i = 0, j = 0; i <= valueLimit; i++, j++)
-        {
-            for (String op : unaryOperators)
-            {
-                String expression = op + i;
-                assertEquals(expectedExpression, expressionTypeOf(expression), message + expression);
-            }
-
-            j += i; // Varies numbers (can be adapted)
-        }
-    }
-
-    @Test
-    public void parserIdentifiesConditionalUnaryExpression()
-    {
-        ExpressionType expectedExpression = ExpressionType.UNARY_EXPRESSION;
-        String message = "Failed to identify conditional unary expression: ";
-
-        String[] keywords = {Syntax.TRUE.getSyntax(), Syntax.FALSE.getSyntax()};
-        String[] unaryOperators = {Syntax.NOT.getSyntax()};
-
-        for (String keyword : keywords)
-        {
-            for (String op : unaryOperators)
-            {
-                String expression = op + keyword;
-                assertEquals(expectedExpression, expressionTypeOf(expression), message + expression);
-            }
-        }
-    }
-
-    @Test
-    public void parserIdentifiesNumericalBinaryExpression()
-    {
-        ExpressionType expectedExpression = ExpressionType.BINARY_EXPRESSION;
-        String message = "Failed to identify numerical binary expression: ";
-
-        int valueLimit = 100;
-        String[] binaryOperators = {Syntax.PLUS.getSyntax(), Syntax.MINUS.getSyntax(), Syntax.STAR.getSyntax(),
-                                    Syntax.SLASH.getSyntax(), Syntax.CARET.getSyntax(), Syntax.PERCENT.getSyntax(),
-                                    Syntax.EQUALS_EQUALS.getSyntax(), Syntax.NOT_EQUALS.getSyntax(),
-                                    Syntax.GREATER.getSyntax(), Syntax.LESS.getSyntax(),
-                                    Syntax.GREATER_EQUALS.getSyntax(), Syntax.LESS_EQUALS.getSyntax()};
-        String[] unaryOperators = {"",
-                                   Syntax.PLUS.getSyntax(), Syntax.MINUS.getSyntax(),
-                                   Syntax.OPEN_PARENTHESIS.getSyntax()};
-
-        for (int i = 0, j = 0; i <= valueLimit; i++, j++)
-        {
-            for (String unaryOp1 : unaryOperators)
-            {
-                String term1 = appendUnaryOperator(unaryOp1, i);
-                for (String unaryOp2 : unaryOperators)
-                {
-                    String term2 = appendUnaryOperator(unaryOp2, i);
-                    for (String op : binaryOperators)
-                    {
-                        String expression = term1 + op + term2;
-                        assertEquals(expectedExpression, expressionTypeOf(expression), message + expression);
-                    }
-                }
-            }
-            j += i; // Varies numbers (can be adapted)
-        }
-    }
-
-    @Test
-    public void parserIdentifiesStringBinaryExpression()
-    {
-        ExpressionType expectedExpression = ExpressionType.BINARY_EXPRESSION;
-        String message = "Failed to identify string binary expression: ";
-
-        String[] binaryOperators = {Syntax.PLUS.getSyntax(), Syntax.EQUALS_EQUALS.getSyntax()};
-        String[] strings = {"\"a\"", "\"string\"", "\"separated string\"", "\"0\"",};
-
-        for (String str : strings)
-        {
-            for (String op : binaryOperators)
-            {
-                String expression = str + op + str;
-                assertEquals(expectedExpression, expressionTypeOf(expression), message + expression);
-            }
-        }
-    }
-
-    @Test
-    public void parserIdentifiesConditionalBinaryExpression()
-    {
-        ExpressionType expectedExpression = ExpressionType.BINARY_EXPRESSION;
-        String message = "Failed to identify conditional binary expression: ";
-
-        String[] keywords = {Syntax.TRUE.getSyntax(), Syntax.FALSE.getSyntax()};
-        String[] binaryOperators = {Syntax.EQUALS_EQUALS.getSyntax(),
-                                    Syntax.NOT_EQUALS.getSyntax(),
-                                    Syntax.AND.getSyntax(),
-                                    Syntax.OR.getSyntax()};
-
-        for (String keyword1 : keywords)
-        {
-            for (String keyword2 : keywords)
-            {
-                for (String op : binaryOperators)
-                {
-                    String expression = keyword1 + ' ' + op + ' ' + keyword2;
-                    assertEquals(expectedExpression, expressionTypeOf(expression), message + expression);
-                }
-            }
-        }
+        binaryCollection.forEach((input, redundant) -> {
+            ExpressionType actual = expressionTypeOf(input);
+            ExpressionType expected = ExpressionType.BINARY_EXPRESSION;
+            assertEquals(expected, actual, message + input);
+        });
     }
 
     @Test
     public void parserIdentifiesParenthesizedExpression()
     {
-        ExpressionType expectedExpression = ExpressionType.PARENTHESIZED_EXPRESSION;
         String message = "Failed to identify parenthesized expression: ";
+        HashMap<String, Object> parenthesizedCollection = TestHub.parenthesizedCollection();
 
-        String[] inputSyntax = {"0", "1", "10000", "123456789", "-1", "-10000", "-123456789",
-                                "10 + 1", "(10 + 5)", "(10 + 5) * 10", "var = true", "true == false"};
-
-        for (String syntax : inputSyntax)
-        {
-            String parenthesizedSyntax = Syntax.OPEN_PARENTHESIS.getSyntax() +
-                                         syntax +
-                                         Syntax.CLOSE_PARENTHESIS.getSyntax();
-            assertEquals(expectedExpression, expressionTypeOf(parenthesizedSyntax), message + syntax);
-        }
+        parenthesizedCollection.forEach((input, redundant) -> {
+            ExpressionType actual = expressionTypeOf(input);
+            ExpressionType expected = ExpressionType.PARENTHESIZED_EXPRESSION;
+            assertEquals(expected, actual, message + input);
+        });
     }
 
     @Test
-    public void parserIdentifiesNameExpression()
+    public void parserIdentifiesIdentifierExpression()
     {
-        ExpressionType expectedExpression = ExpressionType.IDENTIFIER_EXPRESSION;
-        String message = "Failed to identify name expression: ";
+        String message = "Failed to identify identifier expression: ";
+        HashMap<String, Object> identifierCollection = TestHub.identifierCollection();
 
-        String[] inputSyntax = {"a", "aaa", "var", "myVeryLongVariableName"};
-
-        for (String syntax : inputSyntax)
-            assertEquals(expectedExpression, expressionTypeOf(syntax), message + syntax);
+        identifierCollection.forEach((input, redundant) -> {
+            ExpressionType actual = expressionTypeOf(input);
+            ExpressionType expected = ExpressionType.IDENTIFIER_EXPRESSION;
+            assertEquals(expected, actual, message + input);
+        });
     }
 
     @Test
     public void parserIdentifiesAssignmentExpression()
     {
-        ExpressionType expectedExpression = ExpressionType.ASSIGNMENT_EXPRESSION;
         String message = "Failed to identify assignment expression: ";
+        HashMap<String, Object> assignmentCollection = TestHub.assignmentCollection();
 
-        String[] inputSyntax = {"a = 1", "aaa = 10", "var = a", "myVeryLongVariableName = 50", "A = b"};
-
-        for (String syntax : inputSyntax)
-            assertEquals(expectedExpression, expressionTypeOf(syntax), message + syntax);
-    }
-
-    @NotNull
-    @Contract(pure = true)
-    private static String appendUnaryOperator(@NotNull String operator, int value)
-    {
-        if (operator.equals("("))
-            return operator + value + ")";
-        return operator + value;
+        assignmentCollection.forEach((input, redundant) -> {
+            ExpressionType actual = expressionTypeOf(input);
+            ExpressionType expected = ExpressionType.ASSIGNMENT_EXPRESSION;
+            assertEquals(expected, actual, message + input);
+        });
     }
 
     private static ExpressionType expressionTypeOf(String input)
     {
-        Parser parser = createParser(input);
-        //Expression expression = parser.getParseTree().getStatement();
-        return null;
-    }
-
-    @NotNull
-    private static Parser createParser(String input)
-    {
-        return TestHub.createParser(input);
+        Parser parser = TestHub.createParser(input);
+        ExpressionStatement expression = (ExpressionStatement) parser.getParseTree().getStatement();
+        return expression.getExpression().getExpressionType();
     }
 }
