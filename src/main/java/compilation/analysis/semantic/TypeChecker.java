ff1d5d5a18d362d60dcf0a09847e4038f3e87635
Louis Lefevre <louislefev@gmail.com>
2020-06-06 01:03:03 +0100

    Remove issue #51 fix
    Removed temporary fix as it was causing more issues whilst failing to solve the problem to a worthwhile degree.
    
    Opens #51
    Closes #60

--- src/main/java/compilation/analysis/semantic/TypeChecker.java
+++ src/main/java/compilation/analysis/semantic/TypeChecker.java
@@ -212,10 +212,6 @@ public final class TypeChecker
         }
         ObjectType type = symbolTable.get(name).getType();
 
-        // Temporary fix for issue #51. See annotateAssignmentExpression for further implementation.
-        if(symbolTable.get(name).getValue() == null)
-            symbolTable.remove(name);
-
         return new AnnotatedIdentifierExpression(name, type);
     }
 
@@ -228,10 +224,6 @@ public final class TypeChecker
         ObjectType symbolType = symbolTable.contains(name) ? symbolTable.get(name).getType() : NULL_OBJECT;
         ObjectType assignmentType = expression.getObjectType();
         AnnotatedAssignmentOperator operator = TypeBinder.bindAssignmentOperators(assignmentTokenType, symbolType, assignmentType);
-
-        // Temporary fix for issue #51. See annotateIdentifierExpression for further implementation.
-        if (!symbolTable.contains(name))
-            symbolTable.add(name, null, assignmentType);
 
         if (operator == null)
         {
