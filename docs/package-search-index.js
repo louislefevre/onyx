a41a09d2d3476fb7b1c570908fd635fb21c00662
Louis Lefevre <louislefev@gmail.com>
2021-09-15 22:05:22 +0100

    Add docs/

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"compilation"},{"l":"compilation.analysis.lexical"},{"l":"compilation.analysis.semantic"},{"l":"compilation.analysis.syntax"},{"l":"compilation.generation"},{"l":"errors"},{"l":"exceptions"},{"l":"source"},{"l":"symbols"},{"l":"types"},{"l":"ui"},{"l":"ui.main"},{"l":"ui.repl"},{"l":"util"}]
